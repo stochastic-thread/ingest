69163cf5ab7af073fb17c939737d9bbbfb0ba976
Arthur Colle <hello@arthurcolle.com>
2017-01-29 16:17:40 -0500

    Refactored crap code

--- lib/ingest/queue.ex
+++ lib/ingest/queue.ex
@@ -1,16 +1,14 @@
 defmodule Queue do
   use GenServer
+  alias GenServer
 
-  def new(state \\ []) do
-    GenServer.start_link(__MODULE__, state, name: __MODULE__)
-  end
+  def new(state \\ []), do: start_link(__MODULE__, state, name: __MODULE__)
+  def get_state, do: GenServer.call(__MODULE__, :get_state)
+  def enqueue(item), do: GenServer.cast(__MODULE__, {:enqueue, item})
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
 
-
-  def handle_cast({:enqueue, value}, state), do: {:noreply, state ++ [value]}
   def handle_call( :get_state,   _from, state), do: {:reply, state, state}
+  def handle_cast({:enqueue, value}, state), do: {:noreply, state ++ [value]}
   def handle_call( :dequeue, _from, []), do: {:reply, nil, []}
   def handle_call( :dequeue, _from, [item|state]), do: {:reply, item, state}
-  def enqueue(item), do: GenServer.cast(__MODULE__, {:enqueue, item})
-  def get_state, do: GenServer.call(__MODULE__, :get_state)
-  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
 end
--- lib/ingest/stack.ex
+++ lib/ingest/stack.ex
@@ -1,18 +1,16 @@
 defmodule Stack do
   use GenServer
+  alias GenServer
 
-  def new(state \\ []) do
-    GenServer.start_link(__MODULE__, state, name: __MODULE__)
-  end
+  def new(state \\ []), do: start_link(__MODULE__, state, name: __MODULE__)
+  def get_state, do: GenServer.call(__MODULE__, :get_state)
+  def push(item), do: GenServer.cast(__MODULE__, {:push, item})
+  def peek, do: GenServer.call(__MODULE__, :peek)
+  def pop, do: GenServer.call(__MODULE__, :pop)
 
   def handle_call( :get_state,   _from, state), do: {:reply, state, state}
   def handle_cast({:push, item}, state), do: {:noreply, [item | state]}
   def handle_call( :peek,  _from, [item|state]), do: {:reply, item, [item|state]}
   def handle_call( :pop, _from, []), do: {:reply, nil, []}
   def handle_call( :pop, _from, [item|state]), do: {:reply, item, state}
-
-  def get_state, do: GenServer.call(__MODULE__, :get_state)
-  def push(item), do: GenServer.cast(__MODULE__, {:push, item})
-  def peek, do: GenServer.call(__MODULE__, :peek)
-  def pop, do: GenServer.call(__MODULE__, :pop)
 end
